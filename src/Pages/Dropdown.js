509fc3fb291c6809b382b93e15de4692d04ba10b
viralsolanki <viralsolanki2@gmail.com>
2023-11-09 15:28:38 +0530

    changes in department mapping

--- src/Pages/Dropdown.js
+++ src/Pages/Dropdown.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import TableContainer from '@mui/material/TableContainer';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
@@ -9,63 +10,147 @@ import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 
-const options = [
-    { id: 1, name: 'Option 1', valueId: 1 },
-    { id: 2, name: 'Option 2', valueId: 2 },
-    { id: 3, name: 'Option 3', valueId: 1 },
-    { id: 4, name: 'Option 4', valueId: 3 },
+const initialData = [
+  { id: 1, name: 'Option 1', valueId: 1 },
+  { id: 2, name: 'Option 2', valueId: 2 },
+  { id: 3, name: 'Option 3', valueId: 1 },
+  { id: 4, name: 'Option 4', valueId: 3 },
 ];
 
-const data = [
-    { id: 1, valueId: 1, dataName: 'Data for Option 1', dataValue: 'Value 1' },
-    { id: 2, valueId: 2, dataName: 'Data for Option 2', dataValue: 'Value 2' },
-    { id: 3, valueId: 1, dataName: 'Data for Option 3', dataValue: 'Value 1' },
-    { id: 4, valueId: 3, dataName: 'Data for Option 4', dataValue: 'Value 3' },
-    { id: 5, valueId: 2, dataName: 'Data for Option 5', dataValue: 'Value 2' },
-];
+function App() {
+  const [selectedOption1, setSelectedOption1] = useState(null);
+  const [selectedOption2, setSelectedOption2] = useState(null);
+  const [data1, setData1] = useState(initialData);
+  const [data2, setData2] = useState([]);
 
-export default function Dropdown() {
-    const [selectedOption, setSelectedOption] = useState(null);
+  const handleTransfer = (fromTable, toTable, selectedOption) => {
+    if (selectedOption) {
+      const fromTableData = fromTable === 1 ? data1 : data2;
+      const toTableData = fromTable === 1 ? data2 : data1;
+      const newData = fromTableData.filter((item) => item.valueId !== selectedOption.valueId);
+      const selectedOptionData = initialData.filter((item) => item.valueId === selectedOption.valueId);
+      toTableData.push(...selectedOptionData);
+      fromTable === 1 ? setData1(newData) : setData2(newData);
+      fromTable === 1 ? setSelectedOption1(null) : setSelectedOption2(null);
+    }
+  };
 
-    const filteredData = data.filter(
-        (item) => selectedOption && item.valueId === selectedOption.valueId
-    );
+  const handleDelete = (fromTable, id) => {
+    const fromTableData = fromTable === 1 ? data1 : data2;
+    const toTableData = fromTable === 1 ? data2 : data1;
+    const deletedItems = fromTableData.filter((item) => item.valueId === id);
+    if (deletedItems.length > 0) {
+      const newData = fromTableData.filter((item) => item.valueId !== id);
+      toTableData.push(...deletedItems);
+      fromTable === 1 ? setData1(newData) : setData2(newData);
+    }
+  };
 
-    return (
-        <div style={{ margin: '20px' }}>
-            <Autocomplete
-                options={options}
-                getOptionLabel={(option) => option.name}
-                onChange={(event, newValue) => {
-                    setSelectedOption(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} label="Select an option" />}
-            />
+  return (
+    <div style={{ margin: '20px' }}>
+      <div>
+        <Autocomplete
+          options={data1}
+          getOptionLabel={(option) => option.name}
+          value={selectedOption1}
+          onChange={(event, newValue) => {
+            setSelectedOption1(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} label="Select an option" />}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleTransfer(1, 2, selectedOption1)}
+        >
+          Transfer
+        </Button>
+      </div>
 
-            {selectedOption && (
-                <TableContainer component={Paper} style={{ marginTop: '20px' }}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>ID</TableCell>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Value</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {filteredData.map((item) => (
-                                <TableRow key={item.id}>
-                                    <TableCell>{item.id}</TableCell>
-                                    <TableCell>{item.dataName}</TableCell>
-                                    <TableCell>{item.dataValue}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            )}
+      <div style={{ display: 'flex', marginTop: '20px' }}>
+        <TableContainer component={Paper} style={{ flex: 1, marginRight: '20px' }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Value ID</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data1.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell>{item.id}</TableCell>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.valueId}</TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => handleTransfer(1, 2, item)}
+                    >
+                      Transfer
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <div>
+          <Autocomplete
+            options={data2}
+            getOptionLabel={(option) => option.name}
+            value={selectedOption2}
+            onChange={(event, newValue) => {
+              setSelectedOption2(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} label="Select an option" />}
+          />
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => handleDelete(2, selectedOption2 ? selectedOption2.valueId : null)}
+          >
+            Delete
+          </Button>
         </div>
-    );
+
+        <TableContainer component={Paper} style={{ flex: 1 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Value ID</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data2.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell>{item.id}</TableCell>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.valueId}</TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      onClick={() => handleDelete(2, item.valueId)}
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
 }
 
-
+export default App;
--- src/Pages/Sample_Collection_Department_Mapping.js
+++ src/Pages/Sample_Collection_Department_Mapping.js
@@ -23,18 +23,43 @@ import classes from '../assets/css/custom.module.css';
 
 
 const location = [
-    { label: 'Suzie Turn' },
-    { label: 'Suzie Turn' },
-    { label: 'Suzie Turn' },
-    { label: 'Suzie Turn' },
+    { id: 1, name: 'Suzie Turn1', valueId: 1 },
+    { id: 2, name: 'Suzie Turn2', valueId: 2 },
+    { id: 3, name: 'Suzie Turn3', valueId: 1 },
+    { id: 4, name: 'Suzie Turn4', valueId: 3 },
+];
+
+const location_name = [
+    { id: 1, valueId: 1, department: 'Administration', subdepartment: 'Document', dataValue: 'Value 1' },
+    { id: 2, valueId: 2, department: 'Clinical Pathology', subdepartment: 'Clinical Pathology', dataValue: 'Value 2' },
+    { id: 3, valueId: 2, department: 'Clinical Pathology', subdepartment: 'Biochemistry', dataValue: 'Value 2' },
+    { id: 4, valueId: 1, department: 'Imaging', subdepartment: 'X-Ray', dataValue: 'Value 1' },
+    { id: 5, valueId: 1, department: 'Imaging', subdepartment: 'Utrasound', dataValue: 'Value 1' },
+    { id: 6, valueId: 1, department: 'Imaging', subdepartment: 'Mammogram', dataValue: 'Value 1' },
+    { id: 7, valueId: 3, department: 'Haematology & Blood Transfusion', subdepartment: 'Day Care', dataValue: 'Value 3' },
+    { id: 8, valueId: 3, department: 'Haematology & Blood Transfusion', subdepartment: 'Component', dataValue: 'Value 3' },
+    { id: 9, valueId: 3, department: 'Haematology & Blood Transfusion', subdepartment: 'doner unit', dataValue: 'Value 3' },
 ];
 
 const center = [
-    { label: 'Radiology' },
-    { label: 'MRI' },
-    { label: 'X-Ray' },
-    { label: 'Ultra Sound' },
-]
+    { id: 1, name: 'Radiology', valueId: 1 },
+    { id: 2, name: 'MRI', valueId: 2 },
+    { id: 3, name: 'X-Ray', valueId: 1 },
+    { id: 4, name: 'Ultra Sound', valueId: 3 },
+];
+
+const center_data = [
+    { id: 1, valueId: 1, subdepartment: 'Document', dataValue: 'Value 2' },
+    { id: 2, valueId: 2, subdepartment: 'Clinical Pathology', dataValue: 'Value 2' },
+    { id: 3, valueId: 2, subdepartment: 'Biochemistry', dataValue: 'Value 2' },
+    { id: 4, valueId: 1, subdepartment: 'X-Ray', dataValue: 'Value 1' },
+    { id: 5, valueId: 1, subdepartment: 'Utrasound', dataValue: 'Value 1' },
+    { id: 6, valueId: 1, subdepartment: 'Mammogram', dataValue: 'Value 1' },
+    { id: 7, valueId: 3, subdepartment: 'Day Care', dataValue: 'Value 3' },
+    { id: 8, valueId: 3, subdepartment: 'Component', dataValue: 'Value 3' },
+    { id: 9, valueId: 3, subdepartment: 'MRI', dataValue: 'Value 1' },
+];
+
 
 const StyledTableCell = styled(TableCell)(({ }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -56,30 +81,42 @@ const StyledTableRow = styled(TableRow)(({ }) => ({
 
 export default function Sample_Collection_Department_Mapping() {
 
-    const [data, setData] = useState([
-        { id: 1, department: 'Administratio',subdepartment: 'Document' },
-        { id: 2, department: 'Clinical Pathology',subdepartment: 'Clinical Pathology'},
-        { id: 3, department: 'Imaging',subdepartment: 'X-Ray' },
-        { id: 4, department: 'Haematology & Blood Transfusion',subdepartment: 'Day Care' },
-    ]);
+    const [selectedLocation, setSelectedLocation] = useState(null);
+    const [data, setData] = useState(location);
 
 
+    const filteredLocation = location_name.filter(
+        (row) => selectedLocation && row.valueId === selectedLocation.valueId
+    );
 
-    const [selectedData, setSelectedData] = useState([]);
+    const [selectedCenter, setSelectedCenter] = useState(null);
+
+    const filteredCenter = center_data.filter(
+        (row) => selectedCenter && row.valueId === selectedCenter.valueId
+    );
+
+    // const [data, setData] = useState([
+    //     { id: 1, department: 'Administratio', subdepartment: 'Document' },
+    //     { id: 2, department: 'Clinical Pathology', subdepartment: 'Clinical Pathology' },
+    //     { id: 3, department: 'Imaging', subdepartment: 'X-Ray' },
+    //     { id: 4, department: 'Haematology & Blood Transfusion', subdepartment: 'Day Care' },
+    // ]);
+
+    // const [selectedData, setSelectedData] = useState([]);
 
     const handleTransferClick = (row) => {
         // Remove the selected item from the 'data' array
-        setData(data.filter((d) => d.id !== row.id));
+        setData(location_name.filter((d) => d.id !== row.id));
         // Add the selected row to the 'selectedData' array
-        setSelectedData([...selectedData, row]);
+        setSelectedCenter([...selectedCenter, row]);
     };
 
     const handleDeleteClick = (row) => {
         // Remove the selected item from the 'selectedData' array
-        setSelectedData(selectedData.filter((d) => d.id !== row.id));
+        setSelectedCenter(selectedCenter.filter((d) => d.id !== row.id));
         // Add the selected item back to the 'data' array
-        setData([...data, row]);
-      };
+        setData([...center_data, row]);
+    };
 
     return (
         <>
@@ -112,6 +149,10 @@ export default function Sample_Collection_Department_Mapping() {
                                                         disablePortal
                                                         id="combo-box-demo"
                                                         options={location}
+                                                        getOptionLabel={(option) => option.name}
+                                                        onChange={(event, newValue) => {
+                                                            setSelectedLocation(newValue);
+                                                        }}
                                                         renderInput={(params) => <TextField {...params} label="Location Name" />}
                                                     />
 
@@ -122,46 +163,53 @@ export default function Sample_Collection_Department_Mapping() {
                                                             <TableHead>
                                                                 <TableRow>
                                                                     <StyledTableCell>SN</StyledTableCell>
-                                                                    <StyledTableCell>Calories</StyledTableCell>
+                                                                    <StyledTableCell>Department</StyledTableCell>
                                                                     <StyledTableCell>Sub Department</StyledTableCell>
                                                                     <StyledTableCell>ADD</StyledTableCell>
                                                                 </TableRow>
                                                             </TableHead>
                                                             <TableBody>
-                                                                {data.map((row) => (
-                                                                    <StyledTableRow key={row.name}>
-                                                                        <StyledTableCell component="th" scope="row">
-                                                                            {row.id}
-                                                                        </StyledTableCell>
-                                                                        <StyledTableCell>{row.department}</StyledTableCell>
-                                                                        <StyledTableCell>{row.subdepartment}</StyledTableCell>
-                                                                        <StyledTableCell>
-                                                                            <Button variant="contained" sx={{ minWidth: '35px', padding: '6px 8px' }} onClick={() => handleTransferClick(row)}>
-                                                                                <AddIcon />
-                                                                            </Button>
-                                                                        </StyledTableCell>
-                                                                    </StyledTableRow>
-                                                                ))}
+                                                                {selectedLocation && (
+                                                                    filteredLocation.map((row) => (
+                                                                        <StyledTableRow key={row.name}>
+                                                                            <StyledTableCell component="th" scope="row">
+                                                                                {row.id}
+                                                                            </StyledTableCell>
+                                                                            <StyledTableCell>{row.department}</StyledTableCell>
+                                                                            <StyledTableCell>{row.subdepartment}</StyledTableCell>
+                                                                            <StyledTableCell>
+                                                                                <Button variant="contained" sx={{ minWidth: '35px', padding: '6px 8px' }}  
+                                                                                onClick={() => handleTransferClick(row)}>
+                                                                                    <AddIcon />
+                                                                                </Button>
+                                                                            </StyledTableCell>
+                                                                        </StyledTableRow>
+                                                                    ))
+                                                                )}
                                                             </TableBody>
                                                         </Table>
                                                     </TableContainer>
                                                 </Grid>
 
                                                 <Grid item lg={6} sm={6} xs={12} sx={{ marginBottom: "2rem" }}>
-                                                    <Box sx={{display: 'flex'}}>
-                                                    <Autocomplete
+                                                    <Box sx={{ display: 'flex' }}>
+                                                        <Autocomplete
                                                             disablePortal
                                                             id="combo-box-demo"
                                                             options={center}
-                                                            renderInput={(params) => <TextField {...params} label="Center Name"/>}
+                                                            getOptionLabel={(option) => option.name}
+                                                            onChange={(event, newValue) => {
+                                                                setSelectedCenter(newValue);
+                                                            }}
+                                                            renderInput={(params) => <TextField {...params} label="Center Name" />}
                                                             fullWidth
-                                                            sx={{marginRight: '1rem'}}
+                                                            sx={{ marginRight: '1rem' }}
                                                         />
                                                         <Button variant="contained">
                                                             <AddIcon />
                                                         </Button>
                                                     </Box>
-                                                        
+
 
                                                     <Box height={30}></Box>
 
@@ -175,15 +223,18 @@ export default function Sample_Collection_Department_Mapping() {
                                                                 </TableRow>
                                                             </TableHead>
                                                             <TableBody>
-                                                                {selectedData.map((row) => (
-                                                                    <StyledTableRow key={row.name}>
-                                                                        <StyledTableCell component="th" scope="row">
-                                                                            {row.id}
-                                                                        </StyledTableCell>
-                                                                        <StyledTableCell>{row.subdepartment}</StyledTableCell>
-                                                                        <StyledTableCell sx={{ color: 'red', cursor: 'pointer' }}><CloseIcon onClick={() => handleDeleteClick(row)}/></StyledTableCell>
-                                                                    </StyledTableRow>
-                                                                ))}
+                                                                {selectedCenter && (
+                                                                    filteredCenter.map((row) => (
+                                                                        <StyledTableRow key={row.name}>
+                                                                            <StyledTableCell component="th" scope="row">
+                                                                                {row.id}
+                                                                            </StyledTableCell>
+                                                                            <StyledTableCell>{row.subdepartment}</StyledTableCell>
+                                                                            <StyledTableCell sx={{ color: 'red', cursor: 'pointer' }} 
+                                                                            onClick={() => handleDeleteClick(row)}><CloseIcon /></StyledTableCell>
+                                                                        </StyledTableRow>
+                                                                    ))
+                                                                )}
                                                             </TableBody>
                                                         </Table>
                                                     </TableContainer>
