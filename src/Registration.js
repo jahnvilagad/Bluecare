14b275bc6c036c031deaf8fb491c9634c3850758
viralsolanki <viralsolanki2@gmail.com>
2023-11-22 15:33:14 +0530

    validation in login page

--- src/Registration.js
+++ src/Registration.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -15,7 +15,7 @@ import Button from '@mui/material/Button';
 import logo from './assets/img/logo/white.png';
 import classes from './assets/css/custom.module.css';
 import { Typography } from '@mui/material';
-import { inputformelemnet } from './RegisterFormElement';
+import axios from 'axios';
 
 
 function ElevationScroll(props) {
@@ -44,7 +44,56 @@ ElevationScroll.propTypes = {
 };
 
 export default function Login(props) {
+    const [formData, setFormData] = useState({
+        uname: '',
+        email: '',
+        password: '',
+        cpassword: ''
+    })
+    const [errors, setErrors] = useState({})
+    const [valid, setValid] = useState(true)
     const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let isvalid = true;
+        let validationError = {}
+        if (formData.uname === '' || formData.uname === null) {
+            isvalid = false;
+            validationError.uname = 'Username required'
+        }
+        if (formData.email === '' || formData.email === null) {
+            isvalid = false;
+            validationError.email = 'Email required'
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+            isvalid = false;
+            validationError.email = 'Email is not valid'
+        }
+        if (formData.password === '' || formData.password === null) {
+            isvalid = false;
+            validationError.password = 'Password required'
+        } else if (formData.password.length < 6) {
+            isvalid = false;
+            validationError.email = 'password length at least 6 char'
+        }
+        if (formData.cpassword !== formData.password) {
+            isvalid = false;
+            validationError.cpassword = 'Confirm Password not match'
+        }
+        setErrors(validationError)
+        setValid(isvalid)
+
+        if (Object.keys(validationError).length === 0) {
+            axios.post('http://localhost:3001/patient', formData)
+            .then(result =>{
+                navigate('/')
+            })
+            .catch(err => console.log(err))
+
+        }
+    }
+
+    
     return (
         <React.Fragment>
             <Box position='relative'>
@@ -75,15 +124,59 @@ export default function Login(props) {
                                 <Card sx={{ border: '3px solid #172b4d', background: '#f7fafc' }}>
                                     <CardContent sx={{ padding: "1.5rem" }}>
                                         <Box sx={{ width: '100%' }} align='center'>
-                                            <Typography sx={{ fontSize: "1.5rem", fontWeight: "600", color: "#32325d", marginBottom: "1rem" }}>Sign UP</Typography>
+                                            <form onSubmit={handleSubmit}>
+                                                <Typography sx={{ fontSize: "1.5rem", fontWeight: "600", color: "#32325d", marginBottom: "1rem" }}>Sign UP</Typography>
+                                                {
+                                                    valid ? <></> :
+                                                        <span className={classes.TextRed}>
+                                                            {errors.uname}; {errors.email}; {errors.password}; {errors.cpassword}
+                                                        </span>
 
-                                            {
-                                                inputformelemnet.map(input => <TextField {...input} sx={{ marginBottom: '1.5rem' }} />)
-                                            }
+                                                }
+                                                <TextField
+                                                    id="outlined-required"
+                                                    label="Username"
+                                                    placeholder='Enter Username'
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    sx={{ marginBottom: '1.5rem' }}
+                                                    onChange={(event) => setFormData({ ...formData, uname: event.target.value })}
+                                                />
+                                                <TextField
+                                                    id="outlined-required"
+                                                    label="Email"
+                                                    placeholder='Enter Email ID'
+                                                    variant="outlined"
+                                                    type="email"
+                                                    fullWidth
+                                                    sx={{ marginBottom: '1.5rem' }}
+                                                    onChange={(event) => setFormData({ ...formData, email: event.target.value })}
+                                                />
+                                                <TextField
+                                                    id="outlined-required"
+                                                    label="Password"
+                                                    placeholder='Enter Password'
+                                                    variant="outlined"
+                                                    type="password"
+                                                    fullWidth
+                                                    sx={{ marginBottom: '1.5rem' }}
+                                                    onChange={(event) => setFormData({ ...formData, password: event.target.value })}
+                                                />
+                                                <TextField
+                                                    id="outlined-required"
+                                                    label="Confirm Password"
+                                                    placeholder='Enter Confirm Password'
+                                                    variant="outlined"
+                                                    type="password"
+                                                    fullWidth
+                                                    sx={{ marginBottom: '1.5rem' }}
+                                                    onChange={(event) => setFormData({ ...formData, cpassword: event.target.value })}
+                                                />
 
-                                            <Button variant='contained' onClick={() => navigate('/')}>
-                                                Create Account
-                                            </Button>
+                                                <Button variant='contained' type='submit'>
+                                                    Create Account
+                                                </Button>
+                                            </form>
                                         </Box>
                                     </CardContent>
                                 </Card>
