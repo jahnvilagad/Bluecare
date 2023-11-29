791aed90ea7c5afa13e5421422ace3b308ab9844
jahnvi <jahnvi@thewebsitefactory.london>
2023-11-29 22:27:56 +0530

    changes in radio button

--- src/Pages/Dropdown.js
+++ src/Pages/Dropdown.js
@@ -1,34 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid, Paper, Typography } from '@mui/material';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Radio from '@mui/material/Radio';
 // import { withStyles } from '@mui/material/styles';
 // import { withStyles } from '@mui/material/styles';
+import axios from 'axios';
+// import classes from '../assets/css/custom.module.css';
 
 // Custom styles for the radio buttons
 const DailyScheduler = () => {
-  const [selectedMorningSlot, setSelectedMorningSlot] = useState('');
-  const [selectedAfternoonSlot, setSelectedAfternoonSlot] = useState('');
-  const [selectedEveningSlot, setSelectedEveningSlot] = useState('');
+  const [slots, setSlots] = useState({
+    morning: [],
+    afternoon: [],
+    evening: [],
+  });
 
-  const handleMorningChange = (event) => {
-    setSelectedMorningSlot(event.target.value);
-  };
+  useEffect(() => {
+    axios.get('http://localhost:8001/slots')
+      .then(response => {
+        setSlots(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching slots:', error);
+      });
+  }, []);
 
-  const handleAfternoonChange = (event) => {
-    setSelectedAfternoonSlot(event.target.value);
-  };
+  const [selectedSlot, setSelectedSlot] = useState(null);
 
-  const handleEveningChange = (event) => {
-    setSelectedEveningSlot(event.target.value);
-  };
-
-  const availableMorningSlots = ['Slot 1', 'Slot 2', 'Slot 3'];
-  const availableAfternoonSlots = ['Slot A', 'Slot B', 'Slot C'];
-  const availableEveningSlots = ['Slot X', 'Slot Y', 'Slot Z'];
-
-  const radioStyle = {
-    display: 'none', // Hide the default radio button
+  const handleSlotChange = (time, slot) => {
+    setSelectedSlot(slot);
+    // Handle the selected slot based on the time (morning, afternoon, evening)
+    console.log(`Selected ${time} slot: ${slot}`);
   };
 
   const buttonLikeRadio = {
@@ -50,62 +52,42 @@ const DailyScheduler = () => {
     color: '#fff',
   };
 
+  const renderSlots = (time) => (
+    <Paper>
+      <Typography variant="h6">{`${time} Slot`}</Typography>
+      {slots[time].map((slot, index) => (
+        <label
+          key={index}
+          style={{
+            ...buttonLikeRadio,
+            ...(selectedSlot === slot && selectedButton),
+          }}
+        >
+          <FormControlLabel
+            value={slot}
+            control={<Radio style={{ display: 'none' }} />}
+            label={slot}
+            onChange={() => handleSlotChange(time, slot)}
+          />
+          {/* {slot} */}
+        </label>
+      ))}
+    </Paper>
+  );
+
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
         <Typography variant="h4">My Daily Schedule</Typography>
       </Grid>
       <Grid item xs={12} sm={4}>
-        <Paper>
-          <Typography variant="h6">Morning Slot</Typography>
-          {availableMorningSlots.map((slot, index) => (
-            <label key={index} style={{ ...buttonLikeRadio, ...(selectedMorningSlot === slot && selectedButton) }}>
-              <FormControlLabel
-                value={slot}
-                control={<Radio style={radioStyle} />}
-                label={slot}
-                onChange={handleMorningChange}
-                checked={selectedMorningSlot === slot}
-              />
-              {/* {slot} */}
-            </label>
-          ))}
-        </Paper>
-      </Grid>
-      {/* Repeat similar structure for Afternoon and Evening slots */}
-      <Grid item xs={12} sm={4}>
-        <Paper>
-          <Typography variant="h6">Morning Slot</Typography>
-          {availableAfternoonSlots.map((slot, index) => (
-            <label key={index} style={{ ...buttonLikeRadio, ...(selectedAfternoonSlot === slot && selectedButton) }}>
-              <FormControlLabel
-                value={slot}
-                control={<Radio style={radioStyle} />}
-                label={slot}
-                onChange={handleAfternoonChange}
-                checked={selectedAfternoonSlot === slot}
-              />
-              {/* {slot} */}
-            </label>
-          ))}
-        </Paper>
+        {renderSlots('morning')}
       </Grid>
       <Grid item xs={12} sm={4}>
-        <Paper>
-          <Typography variant="h6">Morning Slot</Typography>
-          {availableEveningSlots.map((slot, index) => (
-            <label key={index} style={{ ...buttonLikeRadio, ...(selectedEveningSlot === slot && selectedButton) }}>
-              <FormControlLabel
-                value={slot}
-                control={<Radio style={radioStyle} />}
-                label={slot}
-                onChange={handleEveningChange}
-                checked={selectedEveningSlot === slot}
-              />
-              {/* {slot} */}
-            </label>
-          ))}
-        </Paper>
+        {renderSlots('afternoon')}
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        {renderSlots('evening')}
       </Grid>
     </Grid>
   );
